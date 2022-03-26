e4948531fda12ebe156dc74410719d1d9e7ff206
Jules Michael <julesmichaelgiovanni@gmail.com>
2022-03-26 13:22:49 +0400

    chore: upgrade version

--- docs/docs.go
+++ docs/docs.go
@@ -463,7 +463,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.1.0",
+	Version:          "v0.2.0",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
